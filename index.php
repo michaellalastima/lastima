48520faf1ab999aa26637dc1560d4542b9ce157a
michaellalastima <60122577+michaellalastima@users.noreply.github.com>
2020-11-28 21:14:37 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<html>
+	<head>
+		<title>aniyo</title>
+			<link rel="stylesheet" type="text/css" href="css/style.css"/>
+			<link rel="stylesheet" type="text/js" href="js/style.js"/>
+<div class="burger text-center">
+  <h1></h1>
+  <div class="topBun"><img  src="https://res.cloudinary.com/j0513/image/upload/v1489587412/TOP_Bun_rxyjqd.png" alt="Top bun">
+    </button>  
+    <div id="bunHidden">
+      <h3>Bun Types</h3>
+      <h3>Bun Articles</h3>
+    </div>
+  </div>
+  <div class="lettuce"><img src="https://res.cloudinary.com/j0513/image/upload/v1489587414/Lettuce_cnrbi4.png"alt="Lettuce">
+    <div id="lettuceHidden">
+      <h3>Greens for Burgers</h3>
+    </div>
+  </div>
+  <div class="tomato"><img src="https://res.cloudinary.com/j0513/image/upload/v1489587415/Tomato_yrljtf.png"alt="Tomato">
+    <div id="tomatoHidden">
+      <h3>Tomato Types</h3>
+      <h3>Tomato Articles</h3>
+    </div>
+  </div>
+  <div class="bacon"><img src="https://res.cloudinary.com/j0513/image/upload/v1489587419/Bacon_lbn6ti.png"alt="Bacon">
+  <div id="baconHidden">
+    <h3>Bacon Types</h3>
+    <h3>Bacon Articles</h3>
+  </div>
+  </div>
+  <div class="cheese"><img src="https://res.cloudinary.com/j0513/image/upload/v1489587420/Cheese_hlth6d.png"alt="cheese">
+  <div id="cheeseHidden">
+    <h3>Cheese Types</h3>
+    <h3>Cheese Articles</h3>
+  </div>
+  </div>
+  <div class="patty"><img src="https://res.cloudinary.com/j0513/image/upload/v1489587459/Patty_f1hbxh.png"alt="Patty">
+  <div id="pattyHidden">
+    <h3>Types of Patties</h3>
+    <h3>Burger Patty Articles</h3>
+  </div>
+  </div>
+  <div class="bottomBun"><img src="https://res.cloudinary.com/j0513/image/upload/v1489587461/BOT_Bun_mukbkw.png"alt="Bottom bun"></div>
+</div>
+
+<body>
+		<div class="container">
+			<div class="computer">
+				<div class="computer-body">
+					<div class="computer-screen">
+						<div class="computer-content">
+							<div class="computer-content-block"></div>
+							<div class="computer-content-text"></div>
+							<div class="computer-content-text"></div>
+							<div class="computer-content-block"></div>
+							<div class="computer-content-text"></div>
+							<div class="computer-content-text"></div>
+						</div>
+					</div>
+					<div class="computer-stand"></div>
+				</div>
+			</div>
+			
+			
+<div class="gift">
+  <div class="gift-top"></div>
+  <div class="gift-box"></div>
+</div>
+
+
+<button class="fc spawned" type="button">
+	<div class="fc-part left"></div>
+	<div class="fc-part right"></div>
+	<div class="fc-crumbs">
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+		<div class="fc-crumb"></div>
+	</div>
+	
+</button>			
+			
+			
